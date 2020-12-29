e7e9852ecfec6ac27d078700a84acd7b04fefc45
Jerry <szy0syz@gmail.com>
2020-12-29 16:29:04 +0800

    updates

--- common/build/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./events/base-listener";
-export * from "./events/base-publisher";
-export * from "./events/subjects";
-export * from "./events/screenshot-created-event";
-export * from "./events/screenshot-finished-event";
